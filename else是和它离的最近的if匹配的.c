4aa2ff12470fe45ec235563e0561d1b47d999a91
Hhy220125 <124283752+Hhy220125@users.noreply.github.com>
2023-04-07 15:46:34 +0800

    Add files via upload
    Signed-off-by: Hhy220125 <124283752+Hhy220125@users.noreply.github.com>

---
+++ else是和它离的最近的if匹配的.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+	int a=0;
+	int b=2;
+	if(a==1)
+	{
+		if (b==2)
+		{
+			printf("Hello£¡\n");
+			
+		}
+	}
+	else
+	{
+		printf("ÏÂÎçºÃ£¬¹·×Ó!");
+	}
+	return 0;
+	
+ } 
---
+++ getchar 函数将大写字母转换为小写字母.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	char a,b,c,d,e,m,n,f,g,k;
+	a=getchar();
+	m=a+32;
+	b=getchar();
+	n=b+32;
+	c=getchar();
+	f=c+32;
+	d=getchar();
+	g=d+32;
+	e=getchr();
+	k=e+32;
+	putchar(m);
+	putchar(n);
+	putchar(f);
+	putchar(g);
+	putchar(k);
+	putchar('\n');
+	return 0;
+	 } 
