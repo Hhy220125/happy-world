f3e81400218f4fbcee5cb5e1e69a670bc9d9b46e
Hhy220125 <124283752+Hhy220125@users.noreply.github.com>
2023-04-08 23:36:02 +0800

    Add files via upload
    Signed-off-by: Hhy220125 <124283752+Hhy220125@users.noreply.github.com>

---
+++ 习题3.6 用printf 和putchar 将CHINA转化为GLMRE.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c,d,e,m,n,f,g,k;
+	a='C';
+	m=a+4;
+	b='h';
+	n=b+4;
+	c='i';
+	f=c+4;
+	d='n';
+	g=d+4;
+	e='a';
+	k=e+4;
+	putchar(m);
+	putchar(n);
+	putchar(f);
+	putchar(g);
+	putchar(k);
+	return 0;
+	
+	
+ } 
